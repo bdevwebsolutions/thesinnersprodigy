c6d40e2c4ceac8a1c779e196d8ba6f36d0322bc9
Bjarne Verdonck <bjarneverdonck@Bjarnes-Mac-mini.local>
2023-08-20 12:15:14 +0200

    Temp fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,61 @@
-import '../styles/globals.css'
-import 'react-slideshow-image/dist/styles.css'
+import "../styles/globals.css";
+import "react-slideshow-image/dist/styles.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import '../components/media/media.css';
-import ItemsProvider from '../context/itemsContext';
-import CartProvider from '../context/cartContext';
-import Head from 'next/head';
+import "../components/media/media.css";
+import React from "react";
+import ItemsProvider from "../context/itemsContext";
+import CartProvider from "../context/cartContext";
+import Head from "next/head";
 
 //COMPONENTS
-import NProgress from 'nprogress';
-import Router from 'next/router';
-import 'nprogress/nprogress.css';
+import NProgress from "nprogress";
+import Router from "next/router";
+import "nprogress/nprogress.css";
 
-import BottomBar from '../components/bottom';
+import BottomBar from "../components/bottom";
 
 NProgress.configure({
-  minimum: 0.3,
-  easing: 'ease',
-  speed: 800,
-  showSpinner: false,
-})
+	minimum: 0.3,
+	easing: "ease",
+	speed: 800,
+	showSpinner: false,
+});
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
-
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <meta name="facebook-domain-verification" content="2bs2dwm5ldg9wveypmriguwayczuo5" />
-        <meta name="facebook-domain-verification" content="mi48dok2qu128br1525yx5shsbxd23" />
-      </Head>
-      <CartProvider>
-        <ItemsProvider>
-          <Component {...pageProps} />
-          <BottomBar/>
-        </ItemsProvider>
-      </CartProvider>
-    </>
-  )
+	React.useEffect(() => {
+		window.location.replace("https://the-sinners-prodigy.myshopify.com/");
+	}, []);
+
+	return (
+		<>
+			<Head>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<meta
+					name='facebook-domain-verification'
+					content='2bs2dwm5ldg9wveypmriguwayczuo5'
+				/>
+				<meta
+					name='facebook-domain-verification'
+					content='mi48dok2qu128br1525yx5shsbxd23'
+				/>
+			</Head>
+			<CartProvider>
+				<ItemsProvider>
+					<Component {...pageProps} />
+					<BottomBar />
+				</ItemsProvider>
+			</CartProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
